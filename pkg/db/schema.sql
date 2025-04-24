e72fb0da3c75b8d2d38eebf9118d49c6364cdc7c
Artem Bielikov <artem.bielikov@treepeck.com>
2025-04-24 13:19:52 +0300

    chore: add sql extension to use uuid

--- pkg/db/schema.sql
+++ pkg/db/schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; 
+
 -- Represents a registered human player (or engine).
 CREATE TABLE IF NOT EXISTS player (
 	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
