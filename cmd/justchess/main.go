94bdc2913e400c78a18f87a30471582850bd6305
Artem Bielikov <artem.bielikov@treepeck.com>
2026-03-29 18:48:53 +0000

    security package
    https://developer.mozilla.org/en-US/observatory/analyze?host=justchess.org 120/100 grade

--- cmd/justchess/main.go
+++ cmd/justchess/main.go
@@ -7,6 +7,7 @@ import (
 
 	"justchess/internal/auth"
 	"justchess/internal/db"
+	"justchess/internal/security"
 	"justchess/internal/web"
 	"justchess/internal/ws"
 )
@@ -48,5 +49,5 @@ func main() {
 	authService.RegisterRoutes(mux)
 
 	log.Print("Starting server.")
-	log.Panic(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", mux))
+	log.Panic(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", security.Headers(mux)))
 }
---
+++ internal/security/security.go
@@ -0,0 +1,17 @@
+package security
+
+import "net/http"
+
+// Headers is the middleware that writes recommended security headers for
+// each incomming request.
+func Headers(next http.Handler) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Security-Policy", "default-src 'self'; img-src 'self' https://justchess.org")
+		rw.Header().Add("Strict-Transport-Security", "max-age=63072000")
+		rw.Header().Add("X-Content-Type-Options", "nosniff")
+		rw.Header().Add("X-Robots_Tag", "index, follow")
+		rw.Header().Add("X-Frame-Options", "DENY")
+
+		next.ServeHTTP(rw, r)
+	}
+}
