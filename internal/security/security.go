3e4da9061132180bff1e73f5e53758522ec026f1
Artem Bielikov <artem.bielikov@treepeck.com>
2026-04-17 06:46:40 +0000

    change CSP header to allow WASM instantiation
    All of a sudden the stockfish WASM stopped working with CSP error
    in the browser console. Somehow I figured out that setting
    'wasm-unsafe-eval' in the 'script-src' fixes this problem.
    
    The question is how it was working perfectly fine for about a month...

--- internal/security/security.go
+++ internal/security/security.go
@@ -6,7 +6,7 @@ import "net/http"
 // each incomming request.
 func Headers(next http.Handler) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
-		rw.Header().Add("Content-Security-Policy", "default-src 'self'; img-src 'self' https://justchess.org")
+		rw.Header().Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'wasm-unsafe-eval'")
 		rw.Header().Add("Strict-Transport-Security", "max-age=63072000")
 		rw.Header().Add("X-Content-Type-Options", "nosniff")
 		rw.Header().Add("X-Robots_Tag", "index, follow")
