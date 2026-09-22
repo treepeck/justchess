88fd6c68049c2e998f670ab4d39b01ab197c878a
Artem Bielikov <artem.bielikov@treepeck.com>
2026-09-22 18:52:17 +0300

    new message types and ClientsPerConn const

--- pkg/proto/proto.go
+++ pkg/proto/proto.go
@@ -4,6 +4,9 @@ package proto
 const (
 	// Limit of concurrent TCP connections between WS and Game servers.
 	MaxConns = 10
+	// Limit of concurrent clients over a single TCP connection. If the number
+	// exceeds the limit, a new connection must be opened.
+	ClientsPerConn = 500
 )
 
 // InMessage is a message sent by WebSocket server.
@@ -30,3 +33,9 @@ type Join string
 
 // Leave is sent by WS server to unregister the player from matchmaking pool.
 type Leave string
+
+// Create is sent by JustChess to register a new queue or room.
+type Create string
+
+// Remove is sent by JustChess to unregister a new queue or room.
+type Remove string
